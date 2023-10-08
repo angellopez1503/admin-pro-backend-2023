fea1be524ff80c12378731a995c5d609f3d8be55
Angel Lopez <arturolopez15@hotmail.com>
2023-10-08 02:22:53 -0500

    Seccion 11 - Inicio - CRUD de Médicos y Hospitales

---
+++ controllers/hospitales.js
@@ -0,0 +1,42 @@
+const getHospitales = (req,res) => {
+
+    res.json({
+        ok:true,
+        msg:'getHospitales'
+    })
+}
+
+const crearHospital = (req,res) => {
+
+    res.json({
+        ok:true,
+        msg:'crearHospital'
+    })
+}
+
+const actualizarHospital = (req,res) => {
+
+    res.json({
+        ok:true,
+        msg:'actualizarHospital'
+    })
+}
+
+
+const borrarHospital = (req,res) => {
+
+    res.json({
+        ok:true,
+        msg:'borrarHospital'
+    })
+}
+
+
+
+
+module.exports = {
+    getHospitales,
+    crearHospital,
+    actualizarHospital,
+    borrarHospital
+}
